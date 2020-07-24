9234b56bee52c5ae6e1293e2d37e761aa8b751d3
alex wennerberg <alex@alexwennerberg.com>
2020-07-23 19:02:28 -0500

    Add Hello World and some dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+mod db;
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    db::initialize_tables().unwrap();
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
 }
