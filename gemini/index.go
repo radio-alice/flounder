9384089a06ebccef2df0bc26cca9d00a32eb7364
alex wennerberg <alex@alexwennerberg.com>
2020-08-21 22:27:12 -0700

    Add simple home page template

---
+++ gemini/index.go
@@ -0,0 +1,56 @@
+// A CGI script to render a simple gemini home page
+package main
+
+import (
+  "database/sql"
+  "html/template"
+  "os"
+  "time"
+  // "fmt"
+  _ "github.com/mattn/go-sqlite3"
+)
+// get config file
+
+type File struct {
+    UserName string
+    FileName string
+    UpdatedAt time.Time
+}
+
+func main() {
+  // route ?
+    database, _ := sql.Open("sqlite3", "../flounder.db")
+    t, _ := template.ParseFiles("index.gmi")
+    rows, _ := database.Query(`SELECT user.username, file.user_path, file.updated_at
+FROM file
+JOIN user
+ON file.user_id = user.id
+ORDER BY file.updated_at DESC
+LIMIT 32`)
+    var files []File
+    for rows.Next() {
+      var file File
+      var unixTime int64
+      _ = rows.Scan(&file.UserName, &file.FileName, &unixTime)
+      file.UpdatedAt = time.Unix(unixTime, 0)
+      files = append(files, file)
+    }
+    rows, _ = database.Query(`SELECT username from user`)
+    var users []string
+    for rows.Next() {
+      var user string
+      _ = rows.Scan(&user)
+      users = append(users, user)
+    }
+    data := struct {
+      Domain string
+      Files []File 
+      Users []string
+    }{
+      Domain: "flounder.online",
+      Files: files,
+      Users: users,
+    }
+    _ = t.Execute(os.Stdout, data)
+    // todo err handling
+}
